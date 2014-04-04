7c79e487d27223cf50a4d25e3709b4947d3f90f0
karthikkamarapu32 <karthikkamarapu32@gmail.com>
2014-04-03 18:35:19 -0700

    Initial Commit

---
+++ src/feature_accel_discs.c
@@ -0,0 +1,158 @@
+#include "pebble.h"
+
+#define MATH_PI 3.141592653589793238462
+#define NUM_DISCS 20
+#define DISC_DENSITY 0.25
+#define ACCEL_RATIO 0.05
+#define ACCEL_STEP_MS 50
+
+typedef struct Vec2d {
+  double x;
+  double y;
+} Vec2d;
+
+typedef struct Disc {
+  Vec2d pos;
+  Vec2d vel;
+  double mass;
+  double radius;
+} Disc;
+
+static const uint32_t ANIMAL_LOG_TAGS[3] = { 0x5, 0xb, 0xd };
+static int acc[3];
+static DataLoggingSessionRef logging_session;
+
+
+static Disc discs[NUM_DISCS];
+
+static double next_radius = 3;
+
+static Window *window;
+
+static GRect window_frame;
+
+static Layer *disc_layer;
+
+static AppTimer *timer;
+
+static double disc_calc_mass(Disc *disc) {
+  return MATH_PI * disc->radius * disc->radius * DISC_DENSITY;
+}
+
+static void disc_init(Disc *disc) {
+  GRect frame = window_frame;
+  disc->pos.x = frame.size.w/2;
+  disc->pos.y = frame.size.h/2;
+  disc->vel.x = 0;
+  disc->vel.y = 0;
+  disc->radius = next_radius;
+  disc->mass = disc_calc_mass(disc);
+  next_radius += 0.5;
+}
+
+static void disc_apply_force(Disc *disc, Vec2d force) {
+  disc->vel.x += force.x / disc->mass;
+  disc->vel.y += force.y / disc->mass;
+}
+
+static void disc_apply_accel(Disc *disc, AccelData accel) {
+  Vec2d force;
+  force.x = accel.x * ACCEL_RATIO;
+  force.y = -accel.y * ACCEL_RATIO;
+  disc_apply_force(disc, force);
+}
+
+static void disc_update(Disc *disc) {
+  const GRect frame = window_frame;
+  double e = 0.5;
+  if ((disc->pos.x - disc->radius < 0 && disc->vel.x < 0)
+    || (disc->pos.x + disc->radius > frame.size.w && disc->vel.x > 0)) {
+    disc->vel.x = -disc->vel.x * e;
+  }
+  if ((disc->pos.y - disc->radius < 0 && disc->vel.y < 0)
+    || (disc->pos.y + disc->radius > frame.size.h && disc->vel.y > 0)) {
+    disc->vel.y = -disc->vel.y * e;
+  }
+  disc->pos.x += disc->vel.x;
+  disc->pos.y += disc->vel.y;
+}
+
+static void disc_draw(GContext *ctx, Disc *disc) {
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, GPoint(disc->pos.x, disc->pos.y), disc->radius);
+}
+
+static void disc_layer_update_callback(Layer *me, GContext *ctx) {
+  for (int i = 0; i < NUM_DISCS; i++) {
+    disc_draw(ctx, &discs[i]);
+  }
+}
+
+static void timer_callback(void *data) {
+  AccelData accel = (AccelData) { .x = 0, .y = 0, .z = 0 };
+  
+  accel_service_peek(&accel);
+  
+  acc[0] = accel.x;
+  acc[1] = accel.y;
+  acc[2] = accel.z;
+ 
+  data_logging_log(logging_session, &acc, 3);
+   
+  for (int i = 0; i < NUM_DISCS; i++) {
+    Disc *disc = &discs[i];
+    disc_apply_accel(disc, accel);
+    disc_update(disc);
+  }
+
+  layer_mark_dirty(disc_layer);
+
+  timer = app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect frame = window_frame = layer_get_frame(window_layer);
+
+  disc_layer = layer_create(frame);
+  layer_set_update_proc(disc_layer, disc_layer_update_callback);
+  layer_add_child(window_layer, disc_layer);
+
+  for (int i = 0; i < NUM_DISCS; i++) {
+    disc_init(&discs[i]);
+  }
+}
+
+static void window_unload(Window *window) {
+  layer_destroy(disc_layer);
+  data_logging_finish(logging_session);
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload
+  });
+  window_stack_push(window, true /* Animated */);
+  window_set_background_color(window, GColorBlack);
+
+  logging_session = data_logging_create( ANIMAL_LOG_TAGS[0], DATA_LOGGING_INT, 4, false);
+ 
+  
+  accel_data_service_subscribe(0, NULL);
+
+  timer = app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
+}
+
+static void deinit(void) {
+  accel_data_service_unsubscribe();
+  
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
